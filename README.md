bc667fedf17f81668b121061bb1bb68481d8a345
Ray Deck <ray@raydeck.com>
2020-08-04 08:19:30 -0500

    feat: support a schemas/*.grahpql glob

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 ### Functions
 
 * [mergeDependencies](#mergedependencies)
+* [mergeFromGlobs](#mergefromglobs)
 
 ## Functions
 
@@ -16,7 +17,7 @@
 
 ▸ **mergeDependencies**(`startPath`: string): *Promise‹string›*
 
-*Defined in [index.ts:11](https://github.com/rhdeck/merge-graphql/blob/ff528dd/src/index.ts#L11)*
+*Defined in [index.ts:11](https://github.com/rhdeck/merge-graphql/blob/d14d7f1/src/index.ts#L11)*
 
 Extract and merge graphql schemas from dependencies into a single string.
 
@@ -27,5 +28,23 @@ Gets all from schemas/common and then just the ones for your project (identified
 Name | Type | Default | Description |
 ------ | ------ | ------ | ------ |
 `startPath` | string | process.cwd() | Path to start looking at package.json and associated node_modules  |
+
+**Returns:** *Promise‹string›*
+
+___
+
+###  mergeFromGlobs
+
+▸ **mergeFromGlobs**(`globs`: string[]): *Promise‹string›*
+
+*Defined in [index.ts:36](https://github.com/rhdeck/merge-graphql/blob/d14d7f1/src/index.ts#L36)*
+
+Merge schema files found in an array of glob strings into a single schema file
+
+**Parameters:**
+
+Name | Type | Description |
+------ | ------ | ------ |
+`globs` | string[] | array of glob strings (ex: ./schemas/*.graphql)  |
 
 **Returns:** *Promise‹string›*
--- src/index.ts
+++ src/index.ts
@@ -13,23 +13,29 @@ export async function mergeDependencies(startPath = process.cwd()) {
     readFileSync(join(startPath, "package.json"), { encoding: "utf-8" })
   );
   const allDeps = { ...(dependencies || {}), ...(devDependencies || {}) };
-  const globs =
-    "{" +
-    Object.keys(allDeps)
-      .flatMap((k) => {
-        const base = join(startPath, "node_modules", k, "schemas");
-        if (!existsSync(base)) return [];
-        let out: string[] = [];
-        const commonPath = join(base, "common");
-        if (existsSync(commonPath)) out.push(join(commonPath, "*.graphql"));
-        const namePath = join(base, name);
-        if (existsSync(namePath)) out.push(join(namePath, "*.graphql"));
-        return out;
-      })
-      .filter(Boolean)
-      .join(",") +
-    "}";
-  const out = await mergeGQLSchemas(globs);
+  const globs = Object.keys(allDeps)
+    .flatMap((k) => {
+      const base = join(startPath, "node_modules", k, "schemas");
+      if (!existsSync(base)) return [];
+      let out: string[] = [join(base, "*.graphql")];
+      const commonPath = join(base, "common");
+      if (existsSync(commonPath)) out.push(join(commonPath, "*.graphql"));
+      const namePath = join(base, name);
+      if (existsSync(namePath)) out.push(join(namePath, "*.graphql"));
+      return out;
+    })
+    .filter(Boolean);
+  if (existsSync(join(startPath, "schemas")))
+    globs.push(join(startPath, "schemas", "**", "*.graphql"));
+  return mergeFromGlobs(globs);
+}
+/**
+ * Merge schema files found in an array of glob strings into a single schema file
+ * @param globs array of glob strings (ex: ./schemas/*.graphql)
+ */
+export async function mergeFromGlobs(globs: string[]) {
+  const globString = "{" + globs.join(",") + "}";
+  const out = await mergeGQLSchemas(globString);
   const schema = printSchemaWithDirectives(out);
   return schema;
 }
